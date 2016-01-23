9d6e4a26a3a7c6f0cba051e5926a037b38ed0ae3
David Kroodsma <davidkroodsma@gmail.com>
2016-01-22 17:39:18 -0800

    made inside images more manageable

---
+++ markdown/scripts/resize_images_inside.py
@@ -0,0 +1,19 @@
+import PIL
+from PIL import Image
+
+
+from os import listdir
+from os.path import isfile, join
+
+mypath = "../look_inside_images/"
+mypathout = "../look_inside_images/"
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+basewidth = 600
+for f in onlyfiles:
+	if ".jpg" in f:
+		img = Image.open(mypath+f)
+		wpercent = (basewidth / float(img.size[0]))
+		hsize = int((float(img.size[1]) * float(wpercent)))
+		img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+		img.save(mypathout+f)
