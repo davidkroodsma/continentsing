fbe6a046249f01dd11de631795cb85c3932ff5e6
David Kroodsma <davidkroodsma@gmail.com>
2016-01-23 13:36:46 -0800

    updated inside

--- inside.php
+++ inside.php
@@ -52,9 +52,9 @@
 							<p>A little after 5 a.m., we ride through Alice Lloyd College on Purpose Road, crossing Integrity Lane, then Faith Avenue, then Action, Character, Conscience, Perseverance, Courage, Consecration. At each intersection, in the lighted bubble of a streetlight, are dawn-singing robins and phoebes and cardinals, each getting a jump on their singing day. Beyond the college, silence reigns, except for barking dogs, crowing roosters ... the you-allllllllll of a female barred owl. 
 							<p>All around us warblers announce the dawn: blue-wings stutter a tsi tsi tsi tsi tsi tis tsi zweeeeeeeeeeee ti ti ti; black-and-whites squeeze out a few thin, rapid wese wese notes, then drop to lower notes before ending high; parulas swirl skyward with a complex buzzy song. Each yellow works through a dozen different “sweet” songs; redstarts bounce among two to four songs; hoodeds sing with the same clear quality of their one tawee tawee tawee TEE-o daytime song, but each song now feels more jumbled, and successive songs are always different. Each warbler in his own excited voice, each in his own way, but on one matter they all agree: All airtime  between songs must be filled with emphatic chipping.</p> 
 							<p>About sunrise I hear the mood shift. Blue-wings offer their lazy beee-bzzzzzzzzz daytime song, black-and-whites their deliberate weesee-weesee-weesee, parulas their simpler daytime song as well. Now each yellow, redstart, and hooded delivers only his one special song that he’s saved for once the dawn wave has passed.  </p>
- -->						
 						
-						</div>
+						
+						</div> -->
 					
 					
 					</li>
