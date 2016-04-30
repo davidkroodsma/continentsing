cfee998a9b07be4cf80a204417ff04fb0d588d2a
David Kroodsma <davidkroodsma@gmail.com>
2016-04-30 15:40:39 -0700

    resized images

--- markdown/scripts/resize_images_inside.py
+++ markdown/scripts/resize_images_inside.py
@@ -8,14 +8,19 @@ mypath = "../look_inside_images/"
 mypathout = "../look_inside_images/"
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-basewidth = 600
+basewidth = 450
 for f in onlyfiles:
 	if ".jpg" in f:
 
 		img = Image.open(mypath+f)
-		if img.size[0] > img.size[1]
-
-		wpercent = (basewidth / float(img.size[0]))
-		hsize = int((float(img.size[1]) * float(wpercent)))
-		img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-		img.save(mypathout+f)
+		if img.size[0] > img.size[1]:
+			wpercent = (basewidth / float(img.size[0]))
+			hsize = int((float(img.size[1]) * float(wpercent)))
+			img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+			img.save(mypathout+f)
+		else:
+			baseheight = basewidth
+			hpercent = (baseheight / float(img.size[1]))
+			wsize = int((float(img.size[0]) * float(hpercent)))
+			img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+			img.save(mypathout+f)
